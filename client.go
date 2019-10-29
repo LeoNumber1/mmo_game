e101a4b216b7b76aec80bafa240ee4e0ef070704
liyuan <liyuan@wondersgroup.com>
2019-10-29 17:27:43 +0800

    first commit

---
+++ client.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aceld/zinx/znet"
+	"github.com/golang/protobuf/proto"
+	"io"
+	"mmo_game/pb"
+	"net"
+	"time"
+)
+
+func main() {
+	fmt.Println("Client Test ... start")
+	//3秒之后发起测试请求，给服务端开启服务的机会
+	time.Sleep(3 * time.Second)
+
+	conn, err := net.Dial("tcp", "127.0.0.1:8999")
+	if err != nil {
+		fmt.Println("client start err, exit!")
+		return
+	}
+
+	for {
+		dp := znet.NewDataPack()
+		//msg, _ := dp.Pack(znet.NewMsgPackage(1, []byte("Zinx Client Test Message")))
+		//_, err = conn.Write(msg)
+		//if err != nil {
+		//	fmt.Println("write error err ", err)
+		//	return
+		//}
+
+		//先读出流中的head部分
+		headData := make([]byte, dp.GetHeadLen())
+		_, err = io.ReadFull(conn, headData) //ReadFull 会把msg填充满为止
+		if err != nil {
+			fmt.Println("read head error")
+			break
+		}
+
+		//msg := &pb.BroadCast{}
+		//err = proto.Unmarshal(headData, msg)
+		//if err != nil {
+		//	fmt.Println("-------unmarshal err :", err)
+		//	//return
+		//}
+		//
+		//fmt.Println("msg =", msg)
+
+		//将headData字节流 拆包到msg中
+		msgHead, err := dp.Unpack(headData)
+		if err != nil {
+			fmt.Println("server unpack err:", err)
+			return
+		}
+
+		if msgHead.GetDataLen() > 0 {
+			//msg 是有data数据的，需要再次读取data数据
+			msg := msgHead.(*znet.Message)
+			msg.Data = make([]byte, msg.GetDataLen())
+
+			//根据dataLen从io中读取字节流
+			_, err := io.ReadFull(conn, msg.Data)
+			if err != nil {
+				fmt.Println("server unpack data err:", err)
+				return
+			}
+
+			fmt.Println("==> Recv Msg: ID=", msg.Id, ", len=", msg.DataLen, ", data=", msg.Data)
+			if msg.Id == 1 {
+				msgD := &pb.SyncPid{}
+				err = proto.Unmarshal(msg.Data, msgD)
+				if err != nil {
+					fmt.Println("-------unmarshal err :", err)
+					return
+				}
+				fmt.Println("msg.Data =", msgD)
+			} else if msg.Id == 200 {
+				msgD := &pb.BroadCast{}
+				err = proto.Unmarshal(msg.Data, msgD)
+				if err != nil {
+					fmt.Println("-------unmarshal err :", err)
+					return
+				}
+				fmt.Println("msg.Data =", msgD)
+			}
+		}
+
+		time.Sleep(2 * time.Second)
+	}
+}
---
+++ server.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aceld/zinx/ziface"
+	"github.com/aceld/zinx/znet"
+	"mmo_game/core"
+)
+
+func main() {
+	// 创建服务器句柄
+	s := znet.NewServer()
+
+	//注册客户端连接建立和丢失函数
+	s.SetOnConnStart(OnConnectionAdd)
+
+	// 启动服务
+	s.Serve()
+}
+
+//当客户端建立连接的时候的hook函数
+func OnConnectionAdd(conn ziface.IConnection) {
+	//创建一个玩家
+	player := core.NewPlayer(conn)
+	//同步当前的PlayerID给客户端，走MsgID：1 消息
+	player.SyncPid()
+	//同步当前玩家的初始化坐标信息给客户端，走MsgID：200 消息
+	player.BroadCastStartPosition()
+
+	//====将当前上线玩家添加到worldManager中
+	core.WorldMgrObj.AddPlayer(player)
+	//=============将该连接绑定属性Pid======================
+	conn.SetProperty("pid", player.Pid)
+	//=====================================================
+
+	fmt.Println("======> Player pidId =", player.Pid, " arrived =====")
+}
